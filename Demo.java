ef721d0d47be357aa271141a01e467ee2c538c1a
752408915 <752408915@qq.com>
2018-10-17 16:35:53 +0800

    2

--- Demo.java
+++ Demo.java
@@ -9,4 +9,14 @@ public class Demo {
     public void setName(String name) {
         this.name = name;
     }
+
+    private String pwd;
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
 }
